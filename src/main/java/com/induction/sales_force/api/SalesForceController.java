545d6e0a43b17ba7eda11354c1ce25bef8c22af0
bikas <bikas.kp@sacumen.com>
2023-08-18 16:06:13 +0530

    Added Valid exception message

--- src/main/java/com/induction/sales_force/api/SalesForceController.java
+++ src/main/java/com/induction/sales_force/api/SalesForceController.java
@@ -35,7 +35,7 @@ public class SalesForceController {
     /**
      * create event in the SalesForce system.
      *
-     * @param event               to add event details in SalesForce system.
+     * @param event to add event details in SalesForce system.
      * @param authorizationHeader The Header containing token.
      * @return An event id and success status.
      */
